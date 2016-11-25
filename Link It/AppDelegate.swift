b7fa028c7caf85b46257b36f42e0bf8bbcfef183
Stephen Maitland <s.maitland@me.com>
2016-11-25 19:50:30 +0000

    Initial Commit

---
+++ Link It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Link It
+//
+//  Created by Stephen on 25/11/2016.
+//  Copyright © 2016 Luminator Technology. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
